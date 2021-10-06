b754ccafd17f2ddaa7f67b4fe028eae78858d346
David Abercrombie <abercrombiedj2@gmail.com>
2021-10-06 16:49:22 +0100

    second commit

--- hw_traveller/hw_traveller_start/models/traveller.js
+++ hw_traveller/hw_traveller_start/models/traveller.js
@@ -3,27 +3,44 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  const start = this.journeys.map((journey) => {
+    return journey.startLocation;
+  });
+  return start;
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  const end = this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
+  return end;
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  const filterByTransport = this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  });
+  return filterByTransport;
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  const filterByDistance = this.journeys.filter((journey) => {
+    return journey.distance >= minDistance;
+  });
+  return filterByDistance;
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance;
+  }, 0);
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
+  const transportList = this.journeys.map((journey) => {
+    return journey.transport;
+  });
+  return Array.from(new Set (transportList));
 };
 
 
